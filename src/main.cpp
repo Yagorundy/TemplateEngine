06d31119b4216c88489a1a5b67dedf1031f40999
geepe <geepe@LAPTOP-2FVH51QQ>
2021-12-26 21:13:28 +0200

    Add project files.

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+/**
+*
+* Solution to course project # 4
+* Introduction to programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* Winter semester 2021/2022
+*
+* @author Yavor Tsankov
+* @idnumber 0MI0600095
+* @compiler GCC
+*
+* <main function>
+*
+*/
+
+int main() {
+
+}
